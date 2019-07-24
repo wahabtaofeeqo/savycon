9d3b09ffa68efb34abc84635bd021d38fdf1d079
Adeyinka M. Adefolurin <folurinyinka@gmail.com>
2019-07-24 11:30:52 +0100

    Removed the job title remedy on user's services

--- database/migrations/2019_07_01_100040_create_services_table.php
+++ database/migrations/2019_07_01_100040_create_services_table.php
@@ -16,7 +16,6 @@ class CreateServicesTable extends Migration
         Schema::create('services', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name')->unique();
-            $table->string('title')->unique();
             $table->unsignedBigInteger('category_id');
             $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
             $table->timestamps();
