53ffc96a6ec81158ccb5b140b1b5943339336dad
Adeyinka M. Adefolurin <folurinyinka@gmail.com>
2019-08-15 16:50:03 +0100

    Returned to normal

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,169 @@
+<?php
+
+namespace SavyCon\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use SavyCon\Models\Advert;
+use SavyCon\Models\UserService;
+use SavyCon\Models\UserRequest;
+use SavyCon\Models\User;
+use SavyCon\Models\Category;
+use SavyCon\Models\Service;
+use SavyCon\Models\UserServiceRating;
+
+class PagesController extends Controller
+{
+    public function index()
+    {
+        $featured_services = UserService::inRandomOrder()->where('featured', '1')->with([
+            'user',
+            'service',
+            'service.category'
+        ])->where('active', 1)->get();
+
+        $adverts = Advert::inRandomOrder()->where('layer', 'home')->orWhere('layer', 'all')->limit(6)->get();
+
+    	return view('pages.index', [
+            'featured_services' => $featured_services,
+            'adverts' => $adverts
+        ]);
+    }
+
+    public function services()
+    {
+    	return view('pages.services');
+    }
+
+    public function showService($id)
+    {
+        $service = UserService::withCount('ratings')->
+        with([
+            'user',
+            'service',
+            'service.category'
+        ])->findOrFail($id);
+
+        $alreadyReviewed = null;
+        $canComment = 1;
+
+        if (auth()->user()) {
+            if (auth()->user()->id === $service->id) {
+                $canComment = null;
+            }
+
+            $ratings = auth()->user()->ratings()->where('user_service_id', $service->id)->get();
+            if (count($ratings) > 0) {
+                $alreadyReviewed = 1;
+            }
+        }
+
+        $average_rating = $this->getAverageReview(UserServiceRating::where('user_service_id', $service->id)->get()); 
+
+        return view('pages.services.single')->with([
+            'service' => $service,
+            'canComment' => $canComment,
+            'alreadyReviewed' => $alreadyReviewed,
+            'average_rating' => $average_rating
+        ]);
+    }
+
+    public function userServices($id) 
+    {
+        $user = User::findOrFail($id);
+        $services = UserService::where('user_id', $id)->paginate(1);
+
+        return view('pages.services.user')->with([
+            'user' => $user,
+            'services' => $services
+        ]);
+    }
+
+    public function buyersRequests()
+    {
+        $buyersRequests = UserRequest::inRandomOrder()->with([
+            'user'
+        ])->paginate(20);
+
+        return view('pages.requests.requests')->with([
+            'buyersRequests' => $buyersRequests,
+        ]);
+    }
+
+    public function showBuyersRequest($id)
+    {
+        $buyersRequest = UserRequest::with([
+            'user'
+        ])->findOrFail($id);
+
+        return view('pages.requests.single')->with([
+            'buyersRequest' => $buyersRequest,
+        ]);
+    }
+
+    public function about()
+    {
+        return view('pages.about');
+    }
+
+    public function contact()
+    {
+    	return view('pages.contact');
+    }
+
+    public function terms()
+    {
+    	return view('pages.terms');
+    }
+
+    public function privacyPolicy()
+    {
+    	return view('pages.privacyPolicy');
+    }
+
+    public function howItWorks()
+    {
+        return view('pages.howItWorks');
+    }
+
+    public function help()
+    {
+        return view('pages.help');
+    }
+
+    public function loadCategory($id)
+    {
+        $category = Category::findOrFail($id);
+
+        return view('pages.services.category')->with([
+            'category' => $category,
+        ]);
+    }
+
+    public function loadSubCategory($id)
+    {
+        $subCategory = Service::findOrFail($id);
+
+        return view('pages.services.subCategory')->with([
+            'subCategory' => $subCategory,
+        ]);
+    }
+
+    private function getAverageReview($ratings)
+    {
+        $number_of_reviews = count($ratings);
+        $total = 0;
+
+        foreach ($ratings as $rating) {
+            $total += $rating->stars;
+        }
+
+        if ($number_of_reviews > 0) {
+            $average = $total / $number_of_reviews;
+        } else {
+            $average = 0;
+        }
+
+        return $average;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,111 @@
 Route::get('/', 'PagesController@index')->name('index');
 Route::get('/index', 'PagesController@index');
 
+// Services
+Route::get('/services', 'PagesController@services')->name('services');
+Route::get('/services/user/{id}', 'PagesController@userServices')->where('/id', '([0-9]+)')->name('services.user');
+Route::get('/our-services', 'PagesController@services');
+Route::get('/service/{id}', 'PagesController@showService')->where('/id', '([0-9]+)')->name('service.single');
 
+// Buyers' Requests
+Route::get('/buyers-requests', 'PagesController@buyersRequests')->name('buyerRequests');
+Route::get('/buyers-requests/{id}', 'PagesController@showBuyersRequest')->where('/id', '([0-9]+)')->name('single.buyersRequest');
+Route::get('/requests', 'PagesController@buyersRequests');
+Route::get('/buyersRequests', 'PagesController@buyersRequests');
+
+// Categories & Sub-categories
+Route::get('/category/{id}', 'PagesController@loadCategory')->where('/id', '([0-9]+)')->name('service.category');
+Route::get('/sub-category/{id}', 'PagesController@loadSubCategory')->where('/id', '([0-9]+)')->name('service.service');
+Route::get('/sub/{id}', 'PagesController@loadSubCategory')->where('/id', '([0-9]+)');
+
+// About
+Route::get('/about', 'PagesController@about')->name('about');
+Route::get('/about-us', 'PagesController@about');
+
+// Contact
+Route::get('/contact', 'PagesController@contact')->name('contact');
+Route::get('/contact-us', 'PagesController@contact');
+Route::post('/contact', 'General\ContactEnquiryController@store')->name('contact');
+
+// Help and FAQs
+Route::get('/help-and-faqs', 'PagesController@help')->name('help');
+Route::get('/help', 'PagesController@help');
+Route::get('/faqs', 'PagesController@help');
+
+// Terms of Use
+Route::get('/terms-of-use', 'PagesController@terms')->name('terms');
+Route::get('/terms', 'PagesController@terms');
+Route::get('/terms-and-conditions', 'PagesController@terms');
+
+// How it works
+Route::get('/how-it-works', 'PagesController@howItWorks')->name('howItWorks');
+
+// Privacy policy
+Route::get('/privacy-policy', 'PagesController@privacyPolicy')->name('privacyPolicy');
+Route::get('/privacy', 'PagesController@privacyPolicy');
+
+// Subscription
+Route::post('/subscription', 'General\SubscriptionController@store')->name('subscription');
+
+Auth::routes(['verify' => false]);
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+// Socialite
+Route::get('/social/redirect/{provider}/{role}', 'Auth\SocialController@redirectToProvider')->where('provider', 'twitter|linkedin|google|facebook')->name('social.redirect');
+Route::get('/social/callback/{provider}/{role}', 'Auth\SocialController@handleProviderCallback')->where('provider', 'twitter|linkedin|google|facebook');
+
+Route::middleware(['role:admin'])->group(function () {
+	Route::prefix('admin')->group(function () {		
+		Route::get('/', 'AdminController@index')->name('admin');
+
+		Route::get('/{path}', 'AdminController@index')->where([
+			'/path' => '([A-Z\d-\/_.]+)?',
+		]);
+
+		Route::get('/{path}/{pathTwo}', 'AdminController@index')->where([
+			'/path' => '([A-Z\d-\/_.]+)?',
+			'/pathTwo' => '([A-Z\d-\/_.]+)?',
+		]);
+
+		Route::get('/{path}/{pathTwo}/{id}', 'AdminController@index')->where([
+			'/path' => '([A-Z\d-\/_.]+)?',
+			'/pathTwo' => '([A-Z\d-\/_.]+)?',
+			'/id' => '([0-9]+)?',
+		]);
+	});
+});
+
+Route::middleware(['role:vendor'])->group(function () {
+	Route::prefix('vendor')->group(function () {		
+		Route::get('/', 'VendorController@index')->name('vendor');
+
+		Route::get('/{path}', 'VendorController@index')->where([
+			'/path' => '([A-Z\d-\/_.]+)?',
+		]);
+
+		Route::get('/{path}/{pathTwo}', 'VendorController@index')->where([
+			'/path' => '([A-Z\d-\/_.]+)?',
+			'/pathTwo' => '([A-Z\d-\/_.]+)?',
+		]);
+
+		Route::get('/{path}/{pathTwo}/{id}', 'VendorController@index')->where([
+			'/path' => '([A-Z\d-\/_.]+)?',
+			'/pathTwo' => '([A-Z\d-\/_.]+)?',
+			'/id' => '([0-9]+)?',
+		]);
+	});
+});
+
+Route::middleware(['role:user'])->group(function () {
+	Route::prefix('user')->group(function () {
+		
+		Route::get('/', 'UserController@index')->name('user');
+		Route::get('/{path}', 'UserController@index')->where('/path', '([A-Z\d-\/_.]+)?');
+
+		Route::get('/{path}/{id}', 'UserController@index')->where([
+			'/path' => '([A-Z\d-\/_.]+)?',
+			'/id' => '([0-9]+)?',
+		]);
+	});
+});
