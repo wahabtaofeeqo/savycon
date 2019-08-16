645d4f62af75eb6bf60b9f2b180706b3e8d52770
Adeyinka M. Adefolurin <folurinyinka@gmail.com>
2019-08-16 13:05:50 +0100

    Returned to normal

--- database/migrations/2019_07_01_100040_create_services_table.php
+++ database/migrations/2019_07_01_100040_create_services_table.php
@@ -15,7 +15,7 @@ class CreateServicesTable extends Migration
     {
         Schema::create('services', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('name')->unique();
             $table->unsignedBigInteger('category_id');
             $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
             $table->timestamps();
